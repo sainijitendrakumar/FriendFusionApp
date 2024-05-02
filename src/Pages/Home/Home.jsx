9be8af77984e1d68e344bbbef9a777928e844004
jitendra <er.sainijitendra@gmail.com>
2024-05-02 22:14:38 +0530

    add typos

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,6 +7,28 @@ import { Helmet } from 'react-helmet'
 
 
 function Home() {
+  const Keywords = [
+    "social networking",
+    "online community",
+    "social sharing",
+    "viral content",
+    "digital interaction",
+    "social engagement",
+    "user-generated content",
+    "social media platform",
+    "social interaction",
+    "content discovery",
+    "social media trends",
+    "social influence",
+    "digital conversation",
+    "social sharing platform",
+    "online reactions",
+    "social connection",
+    "trending topics",
+    "social media marketing",
+    "social media management",
+    "social media analytics",
+  ];
     const[data,setData]=useState([])
     
     useEffect(() => {
@@ -45,7 +67,18 @@ function Home() {
           name="description"
           content="friend fusion - The ultimate social media app for sharing your reactions online and engaging with others. Join now!"
         />
+         {/* Add meta keywords */}
+         <meta name="keywords" content={Keywords.join(", ")} />
+         {/* Canonical URL */}
+        <link rel="canonical" href="https://www.example.com" />
+         {/* Robots meta tags */}
+         <meta name="robots" content="index, follow" />
+          {/* Author */}
+        <meta name="author" content="Jitendra kumar saini" />
+        {/* Publisher */}
+        <meta name="publisher" content="jitendra kumar saini" />
       </Helmet>
+      <h1>My Social media App</h1>
     <div className='card pt-16'>
         {data && data.length!==0 && data.map((item)=>(
         <div key={item.$id} className='lala m-3 rounded-md'>
--- src/Pages/Home/HomePostCard.jsx
+++ src/Pages/Home/HomePostCard.jsx
@@ -8,7 +8,7 @@ import parse from 'html-react-parser'
 function HomePostCard({ item }) {
   return (
     <div className='w-full rounded mb-4'>
-      {item.featuredImage && <img className='rounded' src={bucket.getFileView(conf.appwriteBucketId, item.featuredImage)} alt="" />}
+      {item.featuredImage && <img className='rounded' src={bucket.getFileView(conf.appwriteBucketId, item.featuredImage)} alt={item.title} />}
         <div className="p-3">
         <h5 className="mb-2 text-base font-bold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
         <div className="mb-2  font-normal text-gray-900 dark:text-gray-400">{parse(item && item.discription)}</div>
